1bc17f3c2793897359b9f721120a7bd308d5eeba
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-26 20:29:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+const totalBatteries = batteryBatches.reduce(function(sum, val) {
+  return sum + val;
+}, 0);
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +22,52 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce((hash, sentence) => {
+  const sentenceLength = sentence.split(' ').length;
+  if (!hash[sentenceLength]) {
+    hash[sentenceLength] = 0;
+  }
+  hash[sentenceLength]++;
+  return hash
+}, {});
+
+
+
+
+
+
+
+
+const arr = [1, 2, 3, 4, 5];
+
+function sumz(array) {
+  sum = 0
+  array.forEach(el=>{
+    sum = sum + el
+  });
+  return sum;
+}
+
+function devide(array) {
+  devide = 1
+  array.forEach(el=>{
+    devide = devide / el
+  });
+  return devide;
+}
+
+function mul(array) {
+  mul = 1
+  array.forEach(el=>{
+    mul = mul * el
+  });
+  return mul;
+}
+
+
+
+
+
+
+// sf
